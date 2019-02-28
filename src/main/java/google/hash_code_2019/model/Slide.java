8c16e153efb98a7854e7e53d3c2d3872c5ef4f1b
Guillaume Le Biller <guillaume.lebiller@traveldoo.com>
2019-02-28 20:52:34 +0100

    Improve

--- src/main/java/google/hash_code_2019/model/Slide.java
+++ src/main/java/google/hash_code_2019/model/Slide.java
@@ -6,21 +6,28 @@ import java.util.Set;
 
 public class Slide {
     public Photo photo1;
-    public Photo photo2;
+    public Photo photo2 = null;
+    public Set<String> tags;
 
 
     public Slide(Photo photo1, Photo photo2) {
         this.photo1 = photo1;
         this.photo2 = photo2;
+        this.tags = calculateTags();
     }
 
     public Slide(Photo p1) {
         this.photo1 = p1;
+        this.tags = calculateTags();
     }
     public Slide() {
     }
 
     public Set<String> getTags() {
+        return this.tags;
+    }
+
+    private Set<String> calculateTags() {
         Set<String> tags = new HashSet<>();
         if (photo2 != null) {
             tags.addAll(photo2.tags);
@@ -31,9 +38,11 @@ public class Slide {
 
     public void addPhoto2(Photo photo) {
         photo2 = photo;
+        calculateTags();
     }
     public void addPhoto1(Photo photo) {
         photo1 = photo;
+        calculateTags();
     }
 
 }
