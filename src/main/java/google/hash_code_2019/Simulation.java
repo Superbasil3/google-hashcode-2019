2bad6a6df95f990269c9d673d96bee70058236e0
Guillaume Le Biller <guillaume.lebiller@traveldoo.com>
2019-02-28 19:34:19 +0100

    Fix compilation

--- src/main/java/google/hash_code_2019/Simulation.java
+++ src/main/java/google/hash_code_2019/Simulation.java
@@ -11,54 +11,54 @@ public class Simulation {
 
 
     Map<Integer, Photo> mapPhoto = new HashMap<>();
-    Map<String,Integer> repartitionTags = new HashMap<>();
+    Map<String, Integer> repartitionTags = new HashMap<>();
 
-  public int simulate() {
-    int score = 0;
+    public int simulate() {
+        int score = 0;
 
-    int maxinterestFactor = 0;
-      for (Photo p1 : mapPhoto.values()) {
-          Slide s1 = new Slide(p1);
-          for (Photo p2 : mapPhoto.values()) {
-              Slide s2 = new Slide(p2);
-              int interest_factor = interest_factor(s1, s2);
-              maxinterestFactor = Math.max(maxinterestFactor, interest_factor);
-          }
-      }
+        int maxinterestFactor = 0;
+        for (Photo p1 : mapPhoto.values()) {
+            Slide s1 = new Slide(p1);
+            for (Photo p2 : mapPhoto.values()) {
+                Slide s2 = new Slide(p2);
+                int interest_factor = interest_factor(s1, s2);
+                maxinterestFactor = Math.max(maxinterestFactor, interest_factor);
+            }
+        }
 
-      return score;
-  }
+        return score;
+    }
 
-  public static int interest_factor(Slide s1, Slide s2) {
-      int common_tags = 0;
-      int tags_in_s1_but_not_in_s2 = 0;
-      int tags_in_s2_but_not_in_s2 = 0;
-      for (String t : s1.getTags()) {
-          if (s2.getTags().contains(t)) {
-              common_tags++;
-          } else {
-              tags_in_s1_but_not_in_s2++;
-          }
-      }
-      for (String t : s2.getTags()) {
-          if (!s1.getTags().contains(t)) {
-              tags_in_s2_but_not_in_s2++;
-          }
-      }
+    public static int interest_factor(Slide s1, Slide s2) {
+        int common_tags = 0;
+        int tags_in_s1_but_not_in_s2 = 0;
+        int tags_in_s2_but_not_in_s2 = 0;
+        for (String t : s1.getTags()) {
+            if (s2.getTags().contains(t)) {
+                common_tags++;
+            } else {
+                tags_in_s1_but_not_in_s2++;
+            }
+        }
+        for (String t : s2.getTags()) {
+            if (!s1.getTags().contains(t)) {
+                tags_in_s2_but_not_in_s2++;
+            }
+        }
         return Math.min(Math.min(common_tags, tags_in_s1_but_not_in_s2), tags_in_s2_but_not_in_s2);
-  }
+    }
 
-  public void addPhoto(Photo photo ){
-      mapPhoto.put(photo.idPhoto, photo);
-  }
+    public void addPhoto(Photo photo) {
+        mapPhoto.put(photo.idPhoto, photo);
+    }
 
     public void addTagRepartition(Photo photo) {
         photo.tags.stream().forEach(tag -> {
                     if (repartitionTags.containsKey(tag)) {
-                        repartitionTags.put(tag,repartitionTags.get(tag) +1 );
+                        repartitionTags.put(tag, repartitionTags.get(tag) + 1);
 
                     } else {
-                        repartitionTags.put(tag,0);
+                        repartitionTags.put(tag, 0);
                     }
                 }
         );
